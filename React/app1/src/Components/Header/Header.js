cffecdb4bdfa66e1010075505e775d4b2dce6ff0
TheSaGiii <sagithedeveloper@gmail.com>
2022-01-11 09:04:38 +0530

    Code uploaded till 11-01-2022

---
+++ React/app1/src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to React</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, atque
+          quae excepturi vitae esse blanditiis libero facilis in commodi eum
+          suscipit tempora adipisci! Voluptatem odio, voluptas enim vero
+          deleniti quia.
+        </p>
+        <button>Explore More!!!</button>
+      </div>
+    );
+  }
+}
+
+export default Header;
